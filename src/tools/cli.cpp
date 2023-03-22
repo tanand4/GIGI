89466412159e6928263f2928f37ed8d2da9d428c
Tanmay anand <tanand4@hawk.iit.edu>
2023-03-22 16:56:48 -0500

    Changed config file for CLI WALLET experiments

--- src/tools/cli.cpp
+++ src/tools/cli.cpp
@@ -9,59 +9,68 @@
 #include "../core/config.hpp"
 using namespace std;
 
-int main(int argc, char** argv) {
-    
+int main(int argc, char **argv)
+{
+
     json config = getConfig(argc, argv);
     config["showHeaderStats"] = false;
     HostManager hosts(config);
 
     json keys;
-    try {
+    try
+    {
         keys = readJsonFromFile("./keys.json");
-        
+
         PublicWalletAddress keyFromAddress = walletAddressFromPublicKey(stringToPublicKey(keys["publicKey"]));
         PublicWalletAddress statedFromAddress = stringToWalletAddress(keys["wallet"]);
-        if (keyFromAddress != statedFromAddress) {
-            cout<<"Wallet address does not match public key. Keyfile is likely corrupted."<<endl;
-            //return 0;
+        if (keyFromAddress != statedFromAddress)
+        {
+            cout << "Wallet address does not match public key. Keyfile is likely corrupted." << endl;
+            // return 0;
         }
-    } catch(...) {
-        cout<<"Could not read ./keys.json"<<endl;
+    }
+    catch (...)
+    {
+        cout << "Could not read ./keys.json" << endl;
         return 0;
     }
 
     PublicKey publicKey = stringToPublicKey(keys["publicKey"]);
     PrivateKey privateKey = stringToPrivateKey(keys["privateKey"]);
 
-    cout<<"Enter to user wallet:"<<endl;
+    cout << "Enter to user wallet:" << endl;
     string to;
-    cin>> to;
+    cout << "Enter the amount in leaves (NOTE: 1 leaf = 1/10,000 PDN):" << endl;
+    cout << "Enter the mining fee (or 0):" << endl;
+    // cin >> to;
+    to = config["wallet"];
     PublicWalletAddress toWallet = stringToWalletAddress(to);
     PublicWalletAddress fromWallet = walletAddressFromPublicKey(publicKey);
 
-    cout<<"Enter the amount in leaves (NOTE: 1 leaf = 1/10,000 PDN):"<<endl;
     TransactionAmount amount;
-    cin>>amount;
+    amount = config["amount"];
+    // cin >> amount;
 
-    cout<<"Enter the mining fee (or 0):"<<endl;
     TransactionAmount fee;
-    cin>>fee;
+    // cin >> fee;
+    fee = config["miningFees"];
 
     string host = hosts.getGoodHost();
 
-    cout<<"Sending to host : " <<host<<endl;
-    
-    Transaction t(fromWallet, toWallet, amount,publicKey, fee);
+    cout << "Sending to host : " << host << endl;
+
+    Transaction t(fromWallet, toWallet, amount, publicKey, fee);
     t.sign(publicKey, privateKey);
 
-    cout<<"Creating transaction..."<<endl;
-    cout<<"================================================="<<endl;
-    cout<<"TRANSACTION JSON (keep this for your records)"<<endl;
-    cout<<"================================================="<<endl;
-    cout<<t.toJson().dump()<<endl;
-    cout<<"=============================="<<endl;
+    cout << "Creating transaction..." << endl;
+    cout << "=================================================" << endl;
+    cout << "TRANSACTION JSON (keep this for your records)" << endl;
+    cout << "=================================================" << endl;
+    cout << t.toJson().dump() << endl;
+    cout << "==============================" << endl;
     json result = sendTransaction(host, t);
-    cout<<result<<endl;
-    cout<<"Finished."<<endl;
+    cout << result << endl;
+    cout << result[0]["status"] << endl;
+    cout << "Finished." << endl;
     return 0;
 }
